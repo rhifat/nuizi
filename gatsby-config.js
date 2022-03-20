e7805521dcbeb67dda2f8d922addb625aba231dc
Rıfat Sanaç <rifatsanac@Rfat-MacBook-Pro.local>
2022-03-20 22:27:09 +0300

    ilk yükleme

---
+++ gatsby-config.js
@@ -0,0 +1,94 @@
+module.exports = {
+  flags: {
+    DEV_SSR: false
+  },
+  plugins: [
+    {
+      resolve: '@elegantstack/gatsby-theme-flexiblog-news',
+      options: {
+        // Add theme options here. Check documentation for available options.
+        siteUrl: process.env.URL || process.env.VERCEL_URL
+      }
+    }
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    //General Site Metadata
+    title: 'FlexiBlog Theme',
+    name: 'FlexiBlog',
+    description: 'My site description...',
+    address: 'New York, NY',
+    email: 'email@example.com',
+    phone: '+1 (888) 888-8888',
+
+    //Site Social Media Links
+    social: [
+      {
+        name: 'Github',
+        url: 'https://github.com/gatsbyjs'
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/gatsbyjs'
+      },
+      {
+        name: 'Instagram',
+        url: 'https://github.com/gatsbyjs'
+      }
+    ],
+
+    //Header Menu Items
+    headerMenu: [
+      {
+        name: 'Home',
+        slug: '/'
+      },
+      {
+        name: 'Our Team',
+        slug: '/authors'
+      },
+      {
+        name: 'Contact',
+        slug: '/contact'
+      }
+    ],
+
+    //Footer Menu Items (2 Sets)
+    footerMenu: [
+      {
+        title: 'Quick Links',
+        items: [
+          {
+            name: 'Advertise with us',
+            slug: '/contact'
+          },
+          {
+            name: 'About Us',
+            slug: '/about'
+          },
+          {
+            name: 'Contact Us',
+            slug: '/contact'
+          }
+        ]
+      },
+      {
+        title: 'Legal Stuff',
+        items: [
+          {
+            name: 'Privacy Notice',
+            slug: '/'
+          },
+          {
+            name: 'Cookie Policy',
+            slug: '/'
+          },
+          {
+            name: 'Terms Of Use',
+            slug: '/'
+          }
+        ]
+      }
+    ]
+  }
+}
